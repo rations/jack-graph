99d28c930d50e66b9b992b8e780acc580caf92fd
rations <rations@jackgraph.com>
2026-04-02 12:53:31 +1000

    fix midi

--- src/Node.cpp
+++ src/Node.cpp
@@ -8,9 +8,18 @@ Node::Node(const std::string& name, PortType type, PortDirection direction, bool
         std::string client = name.substr(0, colon);
         std::string port_part = name.substr(colon + 1);
         if (client == "system") {
-            auto underscore = port_part.find('_');
-            if (underscore != std::string::npos) {
-                client_name = "system:" + port_part.substr(0, underscore);
+            auto last_underscore = port_part.rfind('_');
+            if (last_underscore != std::string::npos) {
+                std::string suffix = port_part.substr(last_underscore + 1);
+                bool is_number = true;
+                for (char c : suffix) {
+                    if (c < '0' || c > '9') { is_number = false; break; }
+                }
+                if (is_number) {
+                    client_name = "system:" + port_part.substr(0, last_underscore);
+                } else {
+                    client_name = "system:" + port_part;
+                }
             } else {
                 client_name = "system:" + port_part;
             }
